47a0d07c4cd3def9a3e8ce6de90101c041c6da2a
Rezaofc2 <106691452+Rezaofc2@users.noreply.github.com>
2022-06-16 12:20:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0896-0679-0112]
+│ •  [0896-0679-0112]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
